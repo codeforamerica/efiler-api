fa2b300a72e8c3ed1ec6759933ab3ead67210476
Mike Rotondo <mrotondo@codeforamerica.org>
2025-08-08 17:07:04 -0700

    read environment variables for an aws access key when downloading gyr  efiler, and pass those env variables from github secrets in the github action through the dockerfile (#23)
    Co-authored-by: standard-ruby-action[bot] <standard-ruby-action[bot]@users.noreply.github.com>

--- scripts/download_gyr_efiler.rb
+++ scripts/download_gyr_efiler.rb
@@ -1,3 +1,8 @@
+if %w[development test].include? ENV["RACK_ENV"]
+  require "dotenv"
+  Dotenv.load(overwrite: true)
+end
+
 require "zip"
 require "aws-sdk-s3"
 
@@ -10,10 +15,11 @@ download_paths = [
 # If the file already exists, do not re-download.
 exit if download_paths.all? { |p| File.exist?(p) }
 
+credentials = Aws::Credentials.new(ENV["GYR_EFILER_RELEASES_AWS_ACCESS_KEY_ID"], ENV["GYR_EFILER_RELEASES_AWS_SECRET_ACCESS_KEY"])
 download_paths.each do |path|
-  Aws::S3::Client.new.get_object(
+  Aws::S3::Client.new(region: "us-east-1", credentials: credentials).get_object(
     response_target: path,
-    bucket: "gyr-efiler-release",
+    bucket: "gyr-efiler-releases",
     key: File.basename(path)
   )
 end
