8d15d3307a89799581118ac07b402a44af82e164
Jenny Heath <jheath@codeforamerica.org>
2025-08-14 11:05:04 -0700

    add acks spec

---
+++ spec/acks_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require "rack/test"
+require "./acks"
+
+RSpec.describe Acks do
+  it "returns the correct status codes for different values of AcceptanceStatusTxt" do
+    response = Rack::Test::UploadedFile.new("spec/fixtures/irs_acknowledgement.xml", "application/xml")
+
+    expect(described_class.handle_ack_response(response))
+      .to match_array(
+            [
+              ["9999992021197yrv4rvl", :accepted],
+              ["9999992021197yrv4rab", :accepted],
+              ["9999992021197yrv4rcd", :rejected],
+              ["9999992021197yrv4ref", :rejected],
+              ["9999992021197yrv4rgh", :rejected],
+              ["9999992021197yrv4rij", :accepted_but_imperfect],
+              ["9999992021197yrv4rkl", :failed],
+            ]
+          )
+  end
+end
