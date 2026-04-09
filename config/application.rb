8ee407041fee40171ad595b34013c27eefae9fe4
Mike Rotondo <mrotondo@codeforamerica.org>
2026-04-08 18:16:38 -0700

    increase the shutdown timeout so that mef jobs have more of a chance to finish before they get killed after SolidQueue receives SIGTERM

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module EfilerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.solid_queue.shutdown_timeout = 30
   end
 end
